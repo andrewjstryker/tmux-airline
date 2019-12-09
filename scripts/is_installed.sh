38d22fc2ae2b5a4e60dd353c173ea040d3e6f248
Andrew Stryker <axs@sdf.org>
2019-12-08 17:08:56 -0800

    Call grep with -qs
    We don't care about messages, just looking for a match.

--- scripts/is_installed.sh
+++ scripts/is_installed.sh
@@ -3,13 +3,13 @@
 # Functions that check if other plugins are installed
 
 is_online_installed () {
-	[[ ! $( ls $CURRENT_DIR/.. | grep -qs "tmux-online" ) ]]
+	[[ ! $( ls "$CURRENT_DIR/.." | grep -qs "tmux-online" ) ]]
 }
 
 is_cpu_installed () {
-	[[ ! $( ls $CURRENT_DIR/.. | grep -qs "tmux-cpu" ) ]]
+	[[ ! $( ls "$CURRENT_DIR/.." | grep -qs "tmux-cpu" ) ]]
 }
 
 is_battery_installed () {
-	[[ ! $( ls $CURRENT_DIR/.. | grep -qs "tmux-battery" ) ]]
+	[[ ! $( ls "$CURRENT_DIR/.." | grep -qs "tmux-battery" ) ]]
 }
