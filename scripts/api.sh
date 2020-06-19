b7716a1bade5dfca468409f1303542fa6df21c85
Andrew Stryker <axs@sdf.org>
2020-06-19 12:41:25 -0700

    Apply b3p recommended settings

--- scripts/api.sh
+++ scripts/api.sh
@@ -10,7 +10,21 @@
 #
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#-----------------------------------------------------------------------------#
+#
+# Bash 3 Boilerplate
+#
+#-----------------------------------------------------------------------------#
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+
 
 source "$CURRENT_DIR/scripts/shared.sh"
 
